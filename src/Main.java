fe89acb52a46a9be97a13faea603ab3709a8de5a
Jan Sima <simahonza@seznam.cz>
2025-10-21 19:21:32 +0200

    zaklady

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        // Zaklady
+        String pozdrav = "Ahoj svete";
+        String jmeno = "preje Honza";
+        jmeno = pozdrav;
+
         //Toto je pro vypsani do konzole
-        System.out.println("Hello world");
+        System.out.println(pozdrav + " " + jmeno);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        */
 
 
 
